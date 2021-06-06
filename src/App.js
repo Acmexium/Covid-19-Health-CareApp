d52e0e673b39436c4d46f94ba50813955600df05
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-06-06 17:05:39 +0530

    HclAPi integrated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 const App = () => {
   // Initialize HCL SDK API
-  const API = process.env.REACT_APP_API_KEY;
+  const API = process.env.REACT_APP_API;
   const hclAPI = new window.HclAPI({
     apiKey: API, // To replace by a working API key
   });
-  window.HCLSDK.init({
-    apiKey: API,
-  });
+  const params = {
+    first: 50,
+
+    offset: 0,
+
+    country: "ca",
+
+    specialties: ["SP.WCA.75"],
+  };
+
+  hclAPI
+    .activities(params)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
 
   return (
     <div className='App'>
